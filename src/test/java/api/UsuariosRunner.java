5280fb16716b93690c50549a16357e5262c36ba3
Smith <sam.smithangulo@gmail.com>
2026-03-03 00:52:46 +0100

    Actualizacion de maven.yml comentario

--- src/test/java/api/UsuariosRunner.java
+++ src/test/java/api/UsuariosRunner.java
@@ -5,10 +5,7 @@ import com.intuit.karate.junit5.Karate;
 class UsuariosRunner {
 
     @Karate.Test
-    Karate testUsers() {
-        // Ejecuta el feature y fuerza la generación de reportes HTML
-        return Karate.run("classpath:api/usuarios.feature")
-                .outputHtmlReport(true) // <--- ESTO ES CLAVE
-                .outputCucumberJson(true); // Opcional, pero recomendado
+    Karate testAll() {
+        return Karate.run().relativeTo(getClass());
     }
 }
